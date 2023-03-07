36d4c8b8efa8d036381355a070da7b311c541e73
Sam Stout <63696062+samuelwstout@users.noreply.github.com>
2023-03-07 19:40:55 +0000

    finish second part of fizzbuzz

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 for (let i = 1; i < 101; i++) {
-  if (i % 3 == 0) console.log('Fizz');
-  else if (i % 5 == 0) console.log('Buzz');
+  if (i % 3 == 0 && i % 5 == 0) console.log(`${i}: FizzBuzz`);
+  else if (i % 3 == 0) console.log(`${i}: Fizz`);
+  else if (i % 5 == 0) console.log(`${i}: Buzz`);
   else console.log(i);
 }
