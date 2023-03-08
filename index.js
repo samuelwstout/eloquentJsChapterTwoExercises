7f1221a59eb010cb53f31afcd7d62e94aab1babc
Sam Stout <63696062+samuelwstout@users.noreply.github.com>
2023-03-08 00:41:54 +0000

    run through chessboard problem on my own with notes

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-let size = 8;
-let board = "";
+let result = "";
+let size = 40;
 for (let i = 0; i < size; i++) {
   for (let j = 0; j < size; j++) {
+    // for the first row:
+    // 0 - 0 = ' '
+    // 0 - 1 = '#'
+    // 0 - 2 = ' '
+    // 0 - 3 = '#'
+    // 0 - 4 = ' '
+    // 0 - 5 = '#'
+    // 0 - 6 = ' '
+    // 0 - 7 = '#'
+    // for the second row:
+    // 1 - 0 = '#'
+    // 1 - 1 = ' '
+    // 1 - 2 = '#'
+    // 1 - 3 = ' '
+    // 1 - 4 = '#'
+    // 1 - 5 = ' '
+    // 1 - 6 = '#'
+    // 1 - 7 = ' '
+    // and so on...
     if ((i + j) % 2 == 0) {
-      board += ' ';
+      result += ' ';
     } else {
-      board += '#';
+      result += '#';
     }
   }
-  board += '\n';
+  result += '\n';
 }
-console.log(board);
+console.log(result);
